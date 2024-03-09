0fa40603acf2511d8f3d8cddf51d59039c4cc7a9
Gideonkipyegon <gideonkipyegon837@gmail.com>
2024-03-09 10:59:10 +0300

    first commit

---
+++ if.py
@@ -0,0 +1,24 @@
+#IF statement
+age =39
+if age >=39:
+ print('You are old enough to vote')
+ 
+ #ifelese statement
+num=25
+if num<20:
+        print('Number is less than 20')
+else:
+        print('Number is greater than 20')
+
+#elif statement
+grade=85
+if grade>=80:
+        print('A grade')
+elif grade>=70:
+      print('B')
+elif grade>=60:
+      print('C')
+elif grade>=50:
+      print('D')
+else:
+      print('E')
